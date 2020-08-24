484ed839da797e6a32a66321affebeef613ac589
chenny <chenny112@163.com>
2020-08-24 16:23:15 +0800

    commit message

--- utils/dispatcher.py
+++ utils/dispatcher.py
@@ -1,8 +1,19 @@
-def Dispatcher():
+def Dispatcher(sicks:list, flag:bool = False):
     '''
     名称： 调度器
     作用： 管理待筛疾病，将其有序分发给具体的疾病确认器
     输入： 待筛疾病
     输出： 具体疾病确认器
     '''
-    pass
+    for sick in sicks :
+        if not flag:
+            Validator(sick, slots, flag)
+            if flag:
+                break
+        
+    if  not flag:
+        print('根据您的描述，暂时无法确认您的疾病，建议您前往附近的医院做进一步检查')    
+
+if __name__ == "__main__":
+    sick1 =[1]
+    Dispatcher(sick1)
